a3358dc00ff83e5d408f561aaae0e8ba86e97718
atuhon <kinnjiroa@gmail.com>
2024-08-16 00:24:45 +0900

    中断

---
+++ themes/html/archive-menu.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<?php get_header()?>
+
+<body>
+
+
+	<h2 class="pageTitle">メニュー<span>MENU</span></h2>
+
+	<main class="main">
+		<section class="sec">
+			<div class="container">
+				<div class="sec_header">
+					<h2 class="title title-jp">フード</h2>
+					<span class="title title-en">FOOD</span>
+				</div>
+				<div class="row justify-content-center">
+					<?php if(have_posts()):?>
+						<?php while(have_posts()):the_post()?>
+					<div class="col-md-3">
+						<?php get_template_part( "template-parts/loop","menu" ) ?>
+
+					</div>
+					<?php endwhile?>
+					<?php endif;?>
+
+				</div>
+			</div>
+		</section>
+	</main>
+
+
+<?php get_footer()?>
+
+</body>
+
+</html>
---
+++ themes/html/template-parts/loop-menu.php
@@ -0,0 +1,27 @@
+<main class="main">
+    <??>
+    <section class="sec">
+        <div class="container">
+            <div class="sec_header">
+                <h2 class="title title-jp">フード</h2>
+                <span class="title title-en">FOOD</span>
+            </div>
+            <div class="row justify-content-center">
+
+                <div class="col-md-3">
+                    <section class="menu">
+                        <a href="#">
+                            <figure class="menu_pic">
+                                <img src="<?php echo get_template_directory_uri()?>/assets/img/common/noimage_600x400.png" alt="">
+                            </figure>
+                            <h3 class="menu_title">メニュー名が入ります</h3>
+                            <p class="menu_price">800円</p>
+                            <div class="menu_desc">
+                                <p>
+                                    概要が入ります。概要が入ります。概要が入ります。概要が入ります。
+                                </p>
+                            </div>
+                        </a>
+                    </section>
+                </div>
+</main>
